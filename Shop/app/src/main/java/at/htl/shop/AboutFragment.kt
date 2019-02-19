7338093f61d63ddde6979d610ab188b1fa23256a
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-02-19 10:02:05 +0100

    Beginn Section 4 Video 15: Update UI Componentswith NavigationUI

---
+++ Shop/app/src/main/java/at/htl/shop/AboutFragment.kt
@@ -0,0 +1,23 @@
+package at.htl.shop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+}
+
+
+
---
+++ Shop/app/src/main/java/at/htl/shop/ShopFragment.kt
@@ -0,0 +1,21 @@
+package at.htl.shop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ShopFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shop, container, false)
+    }
+
+}
+
